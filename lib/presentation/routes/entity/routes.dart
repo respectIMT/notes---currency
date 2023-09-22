0cbb08dd1cb4e4064812bc8ca403ac4815981c0f
Muhsinjon Ibrohimov <muhsinjonibrohimov2002@gmail.com>
2023-09-22 17:22:27 +0500

    1

---
+++ lib/presentation/routes/entity/routes.dart
@@ -0,0 +1,27 @@
+import 'package:test1/presentation/routes/entity/coordinate.dart';
+
+class Routes implements Coordinate {
+  const Routes._({
+    required this.name,
+    required this.path,
+  });
+
+  final String name;
+  final String path;
+
+  static const home = Routes._(
+    name: 'home',
+    path: '/home',
+  );
+  static const currency = Routes._(
+    name: 'currency',
+    path: '/currency',
+  );
+  static const note = Routes._(
+    name: 'note',
+    path: '/notes',
+  );
+
+  @override
+  String toString() => 'name=$name, path=$path';
+}
